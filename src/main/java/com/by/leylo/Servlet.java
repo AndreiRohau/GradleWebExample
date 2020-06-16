13b89d638fee184241d121de9df10ff72753f451
andreirohau <rohau.andrei@gmail.com>
2020-06-16 17:13:24 +0300

    email

--- src/main/java/com/by/leylo/Servlet.java
+++ src/main/java/com/by/leylo/Servlet.java
@@ -18,12 +18,42 @@ public class Servlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOGGER.info("Servlet#doPost() called");
+        final String strategy = request.getParameter("strategy");
+        LOGGER.info("strategy is : " + strategy);
+        if (strategy.equals("postMultiply")) {
+            multiplyPost(request, response);
+        } else if (strategy.equals("sendEmail")) {
+            sendEmail(request, response);
+        }
+    }
+
+    private void sendEmail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String email = request.getParameter("email");
+        final String subject = request.getParameter("subject");
+        final String text = request.getParameter("text");
+        LOGGER.info("Sender: [" + email + "], subject: [" + subject + "], text: [" + text + "].");
+
+        boolean isSuccessfullySent = sendEmail(email, subject, text);
+
+        request.setAttribute("isEmailSend", isSuccessfullySent);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    private boolean sendEmail(String email, String subject, String text) {
+        boolean isSent = true;
+        // stub
+        // write the email processor service to process emails
+
+        // now we act like every time we successfully send emails
+        return isSent;
+    }
+
+    private void multiplyPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         final String valueToMultiply = request.getParameter("valueToMultiply");
         LOGGER.info(valueToMultiply);
         final int number = Integer.parseInt(valueToMultiply);
         request.setAttribute("calculations", "Your calculations are: " + number + " * " + FACTOR + " = " + number*FACTOR);
         request.getRequestDispatcher("result.jsp").forward(request, response);
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -17,12 +17,36 @@
     <input type="number" name="valueToMultiply" value="10"/>
     <button type="submit" >Run calculations</button>
   </form>
-  <hr/>
+  <br/>
 
   <form method="post" action="pathToThisServlet" >
+    <input hidden type="text" name="strategy" value="postMultiply"/>
     <input type="number" name="valueToMultiply" value="10"/>
     <button type="submit" >Go to result jsp page</button>
   </form>
+
+  <hr/>
+  <br/>
+  <br/>
+  <h3>Email form</h3>
+  <form method="post" action="pathToThisServlet" >
+    <input hidden type="text" name="strategy" value="sendEmail"/>
+    <label>
+      Send to: <input type="email" name="email" value="" required style="margin: 5px"/> <br/>
+    </label>
+    <label>
+      Subject: <input type="text" name="subject" value="" required style="margin: 5px"/><br/>
+    </label>
+    <label>
+      Text: <br/> <textarea name="text" style="height: 100px; width: 220px; margin: 5px" required></textarea><br/>
+    </label>
+
+    <p><%=(request.getAttribute("isEmailSend") == null) ? "" :
+            ((Boolean) request.getAttribute("isEmailSend")) ? "Email is sent" : "Email was not sent"%></p>
+
+    <button type="submit" >Send Email</button>
+  </form>
+
 
 
   </body>
